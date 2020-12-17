58ab44a204f066499047adeb83bff452733ffb66
Kevin <k.mary@live.be>
2020-12-17 15:22:55 +0100

    testing fetch API/display for movies views

--- resources/views/components/movie-card.blade.php
+++ resources/views/components/movie-card.blade.php
@@ -19,10 +19,7 @@
             <span>{{ \Carbon\Carbon::parse($movie['release_date'])->format('d M, Y') }}</span>
         </div>
 
-        <div class="text-gray-400 text-sm">
-            @foreach ($movie['genre_ids'] as $genre)
-                {{ $genres->get($genre) }}@if (!$loop->last), @endif                                  
-            @endforeach
-        </div>
+        <div class="text-gray-400 text-sm">@foreach ($movie['genre_ids'] as $genre){{ $genres->get($genre) }}@if (!$loop->last),@endif @endforeach</div>
+    
     </div>
 </div>
