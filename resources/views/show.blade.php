8eb7169c33740fcb160bb9db2e22c3fb46b76959
Kevin <k.mary@live.be>
2020-12-18 18:44:54 +0100

    Gallery modal for movies show view

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -118,7 +118,7 @@
         </div>
     </div> <!-- end cast section -->
 
-    <div class="movie-images border-b border-gray-800">
+    <div class="movie-images" x-data="{ isOpen: false, image: '' }">
         <div class="container mx-auto px-4 py-16">
             <h2 class="text-4xl font-semibold">
                 Images    
@@ -129,12 +129,40 @@
                     @if ($loop->index > 11) @break @endif 
                     
                     <div class="mt-8">
-                        <a href="#">
+                        <a 
+                            href="#" 
+                            @click.prevent="
+                                isOpen = true
+                                image = 'https://image.tmdb.org/t/p/original{{ $image['file_path'] }}'
+                            "
+                        >
                             <img src="https://image.tmdb.org/t/p/w500{{ $image['file_path'] }}" alt="movie images" class="hover:opacity-75 transition ease-in-out duration-150">
                         </a>
                     </div>                                         
                 @endforeach                
             </div> 
+
+            <template x-if="isOpen">
+                <div style="background-color: rgba(0, 0, 0, .5);" class="fixed top-0 left-0 w-full h-full flex items-center shadow-lg overflow-y-auto">
+                    <div class="container mx-auto lg:px-32 rounded-lg overflow-y-auto">
+                        <div class="bg-gray-900 rounded"  @click.away="isOpen = false">
+                            <div class="flex justify-end pr-4 pt-2">
+                                <button
+                                    class="text-3xl leading-none hover:text-gray-300"
+                                    @click="isOpen = false"
+                                    @keydown.escape.window="isOpen = false"
+                                >
+                                    &times;
+                                </button>
+                            </div>
+                            <div class="modal-body px-8 py-8">
+                                <img :src="image" alt="poster">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            <template
+
         </div>
     </div> <!-- end images gallery -->
 @endsection
